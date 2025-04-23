d4b30fe5692fce89178d41ac5f78ada722caed61
brunamendesfis <bruna.mendes@ufu.br>
2025-04-23 14:07:25 -0300

    Atividade_02_v3

---
+++ Redes_Neurais/Atividade_02/Exemplo01.py
@@ -0,0 +1,4 @@
+from partícula import Particula
+
+particula = Particula(0, 0, 10, 10, 1)
+força = particula.newton(0, - 9.8, 0.1) # m/s²
--- Redes_Neurais/Atividade_02/partícula.py
+++ Redes_Neurais/Atividade_02/partícula.py
@@ -1,4 +1,4 @@
-class partícula():
+class Particula():
 
     def __init__(self, x, y, vx, vy, massa):
         self.x = x
@@ -8,13 +8,27 @@ class partícula():
         self.massa = massa
 
     def newton(self, fx, fy, dt):
+        """ Função para aplicar a Segunda Lei de Newton e atualizar a velocidade e a posição da partícula.
+
+        Parâmetros:
+        
+        fx (float): Calcula a força na direção x (N)
+        fy (float): Calcula a força na direção y (N)
+        dt (float): Intervalo de tempo (s)
+
+        """
+        
+        # Cálculo das acelarações me cada eixo:
+        ax = fx/self.massa
+        ay = fy/self.massa
+        
+        # Atualiza as velocidades:
+        self.vx += ax*dt
+        self.vy += ay*dt
+
+        # Atualiza as posições:
+        self.x = self.vx*dt
+        self.y = self.vy*dt
+
         fx = self.massa * self.vx/dt
         fy = self.massa * self.vy/dt
-        return fx, fy
-    
-x = 0
-y = 0
-vx = 10 # m/s
-vy = 10 # m/s
-m = 1 # kg
-
